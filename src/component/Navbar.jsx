2192dae0a11d50061656e6b0de235698644c50b0
Shradha Mishra <shradhamishra2224@gmail.com>
2024-09-19 16:14:54 +0530

    feat: added navbar

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "7vh",
+        backgroundColor: " #2db92d",
+      }}
+    ></div>
+  );
+};
+
+export default Navbar;
